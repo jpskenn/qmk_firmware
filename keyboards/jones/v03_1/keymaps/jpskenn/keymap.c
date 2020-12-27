0a13f51e8ed1c12561421aa9c908ace5cf915202
Takeshi Nishio <kkeennnn@gmail.com>
2020-12-28 06:11:05 +0900

    Update: Add "HENKAN".

--- keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
+++ keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
@@ -72,6 +72,7 @@ enum custom_keycodes {
 #define LOWER   MO(_LOWER)
 #define NUM     TG(_NUM)
 #define C_SPC   LCTL(KC_SPC)
+#define HENKAN  LGUI(KC_GRV)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -80,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,    KC_LBRC,KC_RBRC,
         KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENT,     KC_QUOT,KC_RCTL,
         S_CAP,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,XXXXXXX,LOWER,
-        XXXXXXX,XXXXXXX,KC_LALT,KC_LGUI,       KC_SPC,         SP_RAI,         KC_RGUI,KC_RALT,XXXXXXX,C_SPC,   C_SPC,   XXXXXXX
+        XXXXXXX,XXXXXXX,KC_LALT,KC_LGUI,       KC_SPC,         SP_RAI,         KC_RGUI,KC_RALT,XXXXXXX,XXXXXXX, HENKAN, XXXXXXX
     ),
     [_WIN] = LAYOUT_all(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
