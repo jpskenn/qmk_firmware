3009808e8044bc26619de7fbb07165e796c0f216
Takeshi Nishio <kkeennnn@gmail.com>
2021-01-05 06:08:12 +0900

    Update: 親指バックスペースをお試し

--- keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
+++ keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
@@ -63,7 +63,9 @@ enum custom_keycodes {
 #define ESC_NUM TD(TD_ESC_NUM)
 #define S_CAP   TD(TD_LSFT_CAPS)
 #define SP_RAI  LT(_RAISE, KC_SPC)
+#define BS_RAI  LT(_RAISE, KC_BSPC)
 #define SP_NRAI LT(_NUM_RAISE, KC_SPC)
+#define BS_NRAI LT(_NUM_RAISE, KC_BSPC)
 #define SP_SFT  MT(MOD_LSFT, KC_SPC)
 #define C_SLSH  RCTL_T(KC_SLSH)
 #define CT_E    LCTL(KC_E)
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,    KC_LBRC,KC_RBRC,
         KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENT,     KC_QUOT,KC_RCTL,
         S_CAP,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,XXXXXXX,LOWER,
-        XXXXXXX,XXXXXXX,KC_LALT,KC_LGUI,       KC_SPC,         SP_RAI,         KC_RGUI,KC_RALT,XXXXXXX,XXXXXXX, HENKAN, XXXXXXX
+        XXXXXXX,XXXXXXX,KC_LALT,KC_LGUI,       KC_SPC,         BS_RAI,         KC_RGUI,KC_RALT,XXXXXXX,XXXXXXX, HENKAN, XXXXXXX
     ),
     [_WIN] = LAYOUT_all(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
@@ -95,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,_______,_______,_______,_______,_______,KC_P7,  KC_P8,  KC_P9,  KC_PPLS,_______,    _______,_______,
         _______,    _______,_______,_______,_______,_______,_______,KC_P4,  KC_P5,  KC_P6,  XXXXXXX,_______,    _______,_______,
         _______,        _______,_______,_______,_______,_______,_______,KC_P1,  KC_P2,  KC_P3,  KC_PENT,_______,_______,_______,
-        _______,_______,_______,_______,        _______,        SP_NRAI,        KC_P0,  KC_PDOT,_______,_______,_______,_______
+        _______,_______,_______,_______,        _______,        BS_NRAI,        KC_P0,  KC_PDOT,_______,_______,_______,_______
     ),
     [_LOWER] = LAYOUT_all(
         KC_PAUS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
