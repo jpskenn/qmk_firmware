c5bdf3133a237818203a410ce211a52fdb30614f
Takeshi Nishio <kkeennnn@gmail.com>
2021-01-26 17:59:26 +0900

    Update: ハイフンの位置を変えて、日本語長音の入力が楽になるかテスト中。

--- keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
+++ keyboards/jones/v03_1/keymaps/jpskenn/keymap.c
@@ -70,6 +70,7 @@ enum custom_keycodes {
 #define C_SLSH  RCTL_T(KC_SLSH)
 #define CT_E    LCTL(KC_E)
 #define CT_A    LCTL(KC_A)
+#define CT_QUO  LCTL_T(KC_QUOT)
 #define ALT_GRV LALT(KC_GRV)
 #define LOWER   MO(_LOWER)
 #define NUM     TG(_NUM)
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT_all(
         ESC_NUM,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,    KC_LBRC,KC_RBRC,
-        KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENT,     KC_QUOT,KC_RCTL,
+        KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_MINS,KC_ENT,     KC_SCLN,CT_QUO,
         KC_LSFT,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,XXXXXXX,LOWER,
         XXXXXXX,XXXXXXX,KC_LALT,KC_LGUI,       KC_SPC,         SP_RAI,         KC_RGUI,KC_RALT,XXXXXXX,XXXXXXX, KC_CAPS,XXXXXXX
     ),
