570d0e9c7bd98639f78547a4e35b1f3743134785
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-17 05:08:08 +0900

    Change in-layer space key function to LT().

--- keyboards/jones/v03_1_col2row/keymaps/jpskenn_split_sp/keymap.c
+++ keyboards/jones/v03_1_col2row/keymaps/jpskenn_split_sp/keymap.c
@@ -72,6 +72,7 @@ enum custom_keycodes {
 // #define S_CAPS   TD(TD_LSFT_CAPS)
 #define SP_LOW  LT(_LOWER, KC_SPC)
 #define SP_RAI  LT(_RAISE, KC_SPC)
+#define SP_ADJ  LT(_ADJUST, KC_SPC)
 // #define BS_RAI  LT(_RAISE, KC_BSPC)
 #define SP_NRAI LT(_NUM_LOWER, KC_SPC)
 // #define BS_NRAI LT(_NUM_LOWER, KC_BSPC)
@@ -122,21 +123,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,     _______,_______,
         _______,    M_PSCR, KC_SLCK,KC_PAUS,_______,_______,KC_GRV, KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,    KC_HOME,KC_VOLD,
         _______,        _______,_______,_______,_______,_______,KC_TILD,KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,KC_END, _______,
-        _______,_______,KC_APP, _______,        _______,        ADJUST,         _______,_______,_______,_______,_______,_______
+        _______,_______,KC_APP, _______,        _______,        SP_ADJ,         _______,_______,_______,_______,_______,_______
     ),
     [_RAISE] = LAYOUT_all(
         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
         KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL,     _______,_______,
         _______,    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_SCLN,KC_QUOT,    KC_PGUP,_______,
         _______,        KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_COLN,KC_DQUO,KC_PGDN,_______,
-        _______,_______,KC_F11, KC_F12,         ADJUST,         _______,        _______,_______,_______,_______,_______,_______
+        _______,_______,KC_F11, KC_F12,         SP_ADJ,         _______,        _______,_______,_______,_______,_______,_______
     ),
     [_NUM_LOWER] = LAYOUT_all(
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,    KC_LBRC,KC_RBRC,
         C_ESC,      KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_MINS, KC_ENT,    KC_UP,  C_QUO,
         _______,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, S_SLSH, KC_LEFT,KC_DOWN,KC_RGHT,
-        _______,_______,_______,_______,        _______,        ADJUST,         _______,_______,_______,_______,_______,_______
+        _______,_______,_______,_______,        _______,        SP_ADJ,         _______,_______,_______,_______,_______,_______
     ),
     [_ADJUST] = LAYOUT_all(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
