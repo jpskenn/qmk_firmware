5d3ab657d9e61777add12050d107c9d53561cd14
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-23 20:46:28 +0900

    Turn off Encoder as default. Turn it on by keymap's setting.

--- keyboards/unison/keymaps/music/rules.mk
+++ keyboards/unison/keymaps/music/rules.mk
@@ -3,9 +3,9 @@ CONSOLE_ENABLE = no
 # Audio feature can cause HUGE firmware size impact.
 # You need to turn off MIDI and Sequencer feature.
 AUDIO_ENABLE = no
+ENCODER_ENABLE = yes
 
 MIDI_ENABLE = yes
 SEQUENCER_ENABLE = yes
 SRC += muse.c
 
-ENCODER_ENABLE = yes
