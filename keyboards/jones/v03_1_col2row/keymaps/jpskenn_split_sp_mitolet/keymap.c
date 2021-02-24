11b5ad78bd0cde2dd218285190cb1629421f677e
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-25 07:23:36 +0900

    change macro name to what it behave.

--- keyboards/jones/v03_1_col2row/keymaps/jpskenn_split_sp_mitolet/keymap.c
+++ keyboards/jones/v03_1_col2row/keymaps/jpskenn_split_sp_mitolet/keymap.c
@@ -74,7 +74,7 @@ enum custom_keycodes {
 #define SP_RAI  LT(_RAISE, KC_SPC)
 #define SP_ADJ  LT(_ADJUST, KC_SPC)
 // #define BS_RAI  LT(_RAISE, KC_BSPC)
-#define SP_NRAI LT(_NUM_LOWER, KC_SPC)
+#define SP_NLOW LT(_NUM_LOWER, KC_SPC)
 // #define BS_NRAI LT(_NUM_LOWER, KC_BSPC)
 // #define SP_SFT  MT(MOD_LSFT, KC_SPC)
 #define C_ESC   LCTL_T(KC_ESC)
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,_______,_______,_______,_______,_______,KC_P7,  KC_P8,  KC_P9,  KC_PPLS,_______,    _______,_______,
         _______,    _______,_______,_______,_______,_______,_______,KC_P4,  KC_P5,  KC_P6,  XXXXXXX,_______,    _______,_______,
         _______,        _______,_______,_______,_______,_______,_______,KC_P1,  KC_P2,  KC_P3,  KC_PENT,_______,_______,_______,
-        _______,_______,_______,_______,        SP_NRAI,        _______,        KC_P0,  KC_PDOT,_______,_______,_______,_______
+        _______,_______,_______,_______,        SP_NLOW,        _______,        KC_P0,  KC_PDOT,_______,_______,_______,_______
     ),
     [_LOWER] = LAYOUT_all(
         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
