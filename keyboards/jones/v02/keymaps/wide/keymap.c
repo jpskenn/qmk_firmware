7dd62a7e9dc6f391b42af76e92d1619b4090173c
Takeshi Nishio <kkeennnn@gmail.com>
2021-05-13 20:32:04 +0900

    Add old v02 keyboard.

---
+++ keyboards/jones/v02/keymaps/wide/keymap.c
@@ -0,0 +1,307 @@
+/* Copyright 2020 Takeshi Nishio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#ifdef RGBLIGHT_ENABLE
+#include "rgblight.h"
+extern rgblight_config_t rgblight_config;
+#endif
+
+
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_number {
+    _MAC = 0,
+    _WIN,
+    _NUM,
+    _LOWER,
+    _RAISE,
+    _NUM_LOWER,
+    _ADJUST
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  MAC = SAFE_RANGE,
+  WIN,
+  M_PSCR,
+  GUI_IME,
+};
+
+// Key Macro
+// #define ESC_NUM TD(TD_ESC_NUM)
+#define ESC_NUM LT(_NUM, KC_ESC)
+#define GRV_NUM LT(_NUM, KC_GRV)
+// #define S_CAPS   TD(TD_LSFT_CAPS)
+#define SP_LOW  LT(_LOWER, KC_SPC)
+#define SP_RAI  LT(_RAISE, KC_SPC)
+#define SP_ADJ  LT(_ADJUST, KC_SPC)
+// #define BS_RAI  LT(_RAISE, KC_BSPC)
+#define SP_NLOW LT(_NUM_LOWER, KC_SPC)
+// #define BS_NRAI LT(_NUM_LOWER, KC_BSPC)
+// #define SP_SFT  MT(MOD_LSFT, KC_SPC)
+#define C_ESC   LCTL_T(KC_ESC)
+// #define C_SPC   LCTL(KC_SPC)
+// #define C_SLSH  RCTL_T(KC_SLSH)
+#define S_SLSH  RSFT_T(KC_SLSH)
+#define S_LEFT  RSFT_T(KC_LEFT)
+// #define CT_E    LCTL(KC_E)
+// #define CT_A    LCTL(KC_A)
+#define C_QUO   LCTL_T(KC_QUOT)
+#define ALT_GRV   LALT(KC_GRV)
+#define LOWER   MO(_LOWER)
+#define ADJUST  MO(_ADJUST)
+#define NUM     TG(_NUM)
+// #define HENKAN  LGUI(KC_GRV)
+#define GUI_JA  LGUI_T(KC_LANG1)
+#define GUI_EN  LGUI_T(KC_LANG2)
+#define ALT_JA  LALT_T(KC_LANG1)
+#define ALT_EN  LALT_T(KC_LANG2)
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_MAC] = LAYOUT(
+        GRV_NUM,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_BSLS,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,
+            KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
+            C_ESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_SCLN,KC_QUOT,KC_H,   KC_J,   KC_K,   KC_L,   KC_MINS,KC_ENT,
+        KC_LSFT,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,KC_RGHT,KC_N,   KC_M,   KC_COMM,KC_DOT, S_SLSH, KC_UP,
+        XXXXXXX,XXXXXXX,ALT_JA, GUI_EN, SP_LOW,             KC_ENT,         SP_RAI, XXXXXXX,GUI_IME,  KC_LEFT,KC_DOWN,KC_RGHT
+    ),
+    [_WIN] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+            _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+            _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,KC_LGUI,KC_LALT,_______,            _______,        SP_RAI, XXXXXXX,ALT_GRV, _______,_______,_______
+    ),
+    [_NUM] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PSLS,KC_PSLS,KC_PSLS,KC_PAST,_______,_______,
+            _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P7,  KC_P8,  KC_P9,  KC_PPLS,_______,
+            _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P4,  KC_P5,  KC_P6,  XXXXXXX,  _______,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P1,  KC_P2,  KC_P3,  XXXXXXX,_______,
+        _______,_______,_______,_______,SP_NLOW,            _______,        _______,_______,KC_P0,  KC_PDOT,_______,_______
+    ),
+    [_LOWER] = LAYOUT(
+        _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_INS, KC_DEL, KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+            KC_ESC, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,_______,_______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+            _______,M_PSCR, KC_SLCK,KC_PAUS,_______,_______,_______,_______,KC_GRV, KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
+        _______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,_______,KC_TILD,KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,
+        _______,_______,KC_APP, _______,_______,            _______,        SP_ADJ, _______,_______,_______,_______,_______
+    ),
+    [_RAISE] = LAYOUT(
+        _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,_______,_______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+            KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   _______,_______,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL,
+            _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  _______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_SCLN,KC_QUOT,
+        _______,_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_COLN,KC_DQUO,
+        _______,_______,_______,_______,SP_ADJ,             _______,        _______,_______,_______,_______,_______,_______
+    ),
+    [_NUM_LOWER] = LAYOUT(
+        KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_GRV, KC_BSLS,KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,
+            KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
+            C_ESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_SCLN,KC_QUOT,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT,
+        KC_LSFT,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,KC_RGHT,KC_N,   KC_M,   KC_COMM,KC_DOT, S_SLSH, KC_UP,
+            _______,_______,_______,_______,_______,        _______,        SP_ADJ, _______,_______,KC_LEFT,KC_DOWN,KC_RGHT
+    ),
+    [_ADJUST] = LAYOUT(
+        _______,RGB_HUI,RGB_SAI,RGB_VAI,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+            _______,MAC,    WIN,    _______,RESET,  _______,_______,_______,RGB_HUI,RGB_SAI,RGB_VAI,_______,RGB_RMOD,_______,
+            _______,_______,_______,_______,_______,_______,_______,_______,RGB_HUD,RGB_SAD,RGB_VAD,RGB_TOG,RGB_MOD, _______,
+        KC_CAPS,KC_CAPS,_______,_______,_______,_______,_______,_______,_______,NUM,    MAC,    _______,_______,_______,_______,
+            _______,_______,_______,_______,_______,        _______,         _______,_______,_______,_______,_______,_______
+    )
+};
+
+uint16_t key_timer;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MAC: // Change default ayer --> Write to EEPROM
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_MAC);
+            }
+            return false;
+            break;
+        case WIN: // Change default ayer --> Write to EEPROM
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_WIN);
+            }
+            return false;
+            break;
+        case M_PSCR: // Mac's advanced screen capture
+            switch(biton32(default_layer_state)) {
+                case _WIN:
+                    if (record->event.pressed) {
+                        tap_code(KC_PSCR);
+                    }
+                    break;
+                default:
+                    if (record->event.pressed) {
+                        tap_code16(LSFT(LGUI(KC_5)));
+                    }
+                    break;
+            }
+            return false;
+            break;
+        case GUI_IME: // self made Mod-Tap with toggle IME
+            switch(biton32(default_layer_state)) {
+                case _MAC: // tap to toggle IME, hold to RGUI(=RCMD)
+                    if (record->event.pressed) {
+                        key_timer = timer_read();
+                        register_code(KC_RGUI);
+                    } else {
+                        unregister_code(KC_RGUI);
+
+                        if (timer_elapsed(key_timer) < TAPPING_TERM) {
+                            SEND_STRING(SS_LCMD(SS_LALT(SS_TAP(X_SPC))));
+                        }
+                    }
+                    break;
+                case _WIN: // tap to toggle IME, hold to RALT
+                    if (record->event.pressed) {
+                        key_timer = timer_read();
+                        register_code(KC_RALT);
+                    } else {
+                        unregister_code(KC_RALT);
+
+                        if (timer_elapsed(key_timer) < TAPPING_TERM) {
+                            SEND_STRING(SS_LALT(SS_TAP(X_GRV)));
+                        }
+                    }
+                    break;
+            }
+            return false;
+            break;
+        default:
+            break;
+    }
+    return true;
+}
+
+//------------------------------------------------------------------------------
+// RGB Light settings
+#ifdef RGBLIGHT_LAYERS
+
+// 1st LED
+const rgblight_segment_t PROGMEM my_mac_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 1, HSV_WHITE}
+);
+const rgblight_segment_t PROGMEM my_win_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 1, HSV_BLUE}
+);
+const rgblight_segment_t PROGMEM my_num_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 1, HSV_YELLOW}
+);
+
+// 2nd LED
+const rgblight_segment_t PROGMEM my_caps_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_MAGENTA}
+);
+
+const rgblight_segment_t PROGMEM my_lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_GREEN}
+);
+
+const rgblight_segment_t PROGMEM my_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_CYAN}
+);
+
+const rgblight_segment_t PROGMEM my_num_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_GOLD}
+);
+
+const rgblight_segment_t PROGMEM my_adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_RED}
+);
+
+
+// Define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_mac_layer,
+    my_win_layer,
+    my_num_layer,
+    my_caps_layer,
+    my_lower_layer,
+    my_raise_layer,
+    my_num_raise_layer,
+    my_adjust_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+// Enabling and disabling lighting layers
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(2, layer_state_cmp(state, _NUM));
+    rgblight_set_layer_state(4, layer_state_cmp(state, _LOWER));
+    rgblight_set_layer_state(5, layer_state_cmp(state, _RAISE));
+    rgblight_set_layer_state(6, layer_state_cmp(state, _NUM_LOWER));
+    rgblight_set_layer_state(7, layer_state_cmp(state, _ADJUST));
+
+    return state;
+}
+
+// Enabling and disabling lighting layers for default layer
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, layer_state_cmp(state, _MAC));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _WIN));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _NUM));
+
+    return state;
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(3, led_state.caps_lock);
+    return true;
+}
+#endif
+
+//------------------------------------------------------------------------------
+// TEST LEDs
+// void keyboard_post_init_user(void) {
+//     rgblight_enable_noeeprom();
+//     rgblight_mode_noeeprom(RGBLIGHT_MODE_RGB_TEST);
+// }
+
+//------------------------------------------------------------------------------
+// Rotary Encoder
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    }
+}
+
+
+//------------------------------------------------------------------------------
+/*
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool led_update_user(led_t led_state) {
+    return true;
+}
+*/
