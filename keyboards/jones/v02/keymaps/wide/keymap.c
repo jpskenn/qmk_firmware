f97005113714c2e3be70f57d36e029c2572a43bd
Takeshi Nishio <kkeennnn@gmail.com>
2021-05-15 13:01:25 +0900

    update keymap.

--- keyboards/jones/v02/keymaps/wide/keymap.c
+++ keyboards/jones/v02/keymaps/wide/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
             C_ESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_SCLN,KC_QUOT,KC_H,   KC_J,   KC_K,   KC_L,   KC_MINS,KC_ENT,
         KC_LSFT,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,KC_RGHT,KC_N,   KC_M,   KC_COMM,KC_DOT, S_SLSH, KC_UP,
-        XXXXXXX,XXXXXXX,ALT_JA, GUI_EN, SP_LOW,             KC_ENT,         SP_RAI, XXXXXXX,GUI_IME,  KC_LEFT,KC_DOWN,KC_RGHT
+        XXXXXXX,XXXXXXX,ALT_JA, GUI_EN, SP_LOW,             KC_BSPC,         SP_RAI, XXXXXXX,GUI_IME,  KC_LEFT,KC_DOWN,KC_RGHT
     ),
     [_WIN] = LAYOUT(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,KC_LGUI,KC_LALT,_______,            _______,        SP_RAI, XXXXXXX,ALT_GRV, _______,_______,_______
     ),
     [_NUM] = LAYOUT(
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PSLS,KC_PSLS,KC_PSLS,KC_PAST,_______,_______,
+        KC_GRV,_______,_______,_______,_______,_______,_______,_______,_______,KC_PSLS,KC_PSLS,KC_PSLS,KC_PAST,_______,_______,
             _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P7,  KC_P8,  KC_P9,  KC_PPLS,_______,
             _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P4,  KC_P5,  KC_P6,  XXXXXXX,  _______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P1,  KC_P2,  KC_P3,  XXXXXXX,_______,
@@ -100,10 +100,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,_______,_______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
             _______,M_PSCR, KC_SLCK,KC_PAUS,_______,_______,_______,_______,KC_GRV, KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
         _______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,_______,KC_TILD,KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,
-        _______,_______,KC_APP, _______,_______,            _______,        SP_ADJ, _______,_______,_______,_______,_______
+        _______,_______,_______,_______,_______,            _______,        SP_ADJ, _______,_______,_______,_______,_______
     ),
     [_RAISE] = LAYOUT(
-        _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,_______,_______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,_______,_______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
             KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   _______,_______,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL,
             _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  _______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_SCLN,KC_QUOT,
         _______,_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_COLN,KC_DQUO,
