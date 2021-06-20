9df9ad23b049aa23e734b837c7b8e10a284bfb6b
Takeshi Nishio <kkeennnn@gmail.com>
2021-06-20 14:54:19 +0900

    Add firmsize reduction settngs.

--- keyboards/jones/v04/keymaps/jpskenn/config.h
+++ keyboards/jones/v04/keymaps/jpskenn/config.h
@@ -1,12 +1,33 @@
 #pragma once
 
+// Change LED count.
 // #ifdef RGB_DI_PIN
 //     #undef RGBLED_NUM
 //     #define RGBLED_NUM 4 // Left(2) + Right(2)
 // #endif
 
+// Disable music mode to reduce firmware size.
+// +2.3kb
+#ifdef AUDIO_ENABLE
+//   #undef MUSIC_MAP
+//   #define NO_MUSIC_MODE
+//   #define SONG_CAPS_ON SONG(CAPS_LOCK_ON_SOUND)
+//   #define SONG_CAPS_OFF SONG(CAPS_LOCK_OFF_SOUND)
+#endif
+
+// Disable unused encoder pad to reduce firmware size.
+// Override keyboard's config.h : Left encoder only
+// +0.3kb
+#undef ENCODERS_PAD_A
+#undef ENCODERS_PAD_B
+#define ENCODERS_PAD_A { F5 }
+#define ENCODERS_PAD_B { F4 }
+
+// Use 24LC64 EEPROM
 #define EEPROM_I2C_24LC64
 
+// Set MAX_ADDR to specify actual EEPROM size.
+// If it's not set, EEPROM size will be set ATmega32u4's 1023.
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 8191
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7 // default:4
