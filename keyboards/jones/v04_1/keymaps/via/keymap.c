367c1d520f4f1f01c2fb9192dcb1dfe05de0c9c9
Takeshi Nishio <kkeennnn@gmail.com>
2021-07-22 11:33:26 +0900

    Change right rotary encoder default.

--- keyboards/jones/v04_1/keymaps/via/keymap.c
+++ keyboards/jones/v04_1/keymaps/via/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_LCTL,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_SCLN,  KC_QUOT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    KC_ENT,
         KC_LSFT,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_LEFT,  KC_RGHT,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,    KC_SLSH,
         KC_MUTE,  KC_LANG2, KC_LGUI,     KC_LALT,       SP_LOW,        KC_ENT,KC_ENT, KC_BSPC,SP_RAI,KC_RALT,      KC_RALT,     KC_LEFT,  KC_DOWN,  KC_RGHT,
-        KC_VOLD,  KC_VOLU,                                                                                                                KC_PGDN,  KC_PGUP
+        KC_VOLD,  KC_VOLU,                                                                                                                KC_VOLD,  KC_VOLU
     ),
     [_LOWER] = LAYOUT(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
