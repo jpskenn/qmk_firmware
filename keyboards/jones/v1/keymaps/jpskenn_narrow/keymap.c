4d59585f2de5dd2067b5d8602e9b14d320340407
Takeshi Nishio <kkeennnn@gmail.com>
2021-08-29 09:35:36 +0900

    Add underglow effect range.

--- keyboards/jones/v1/keymaps/jpskenn_narrow/keymap.c
+++ keyboards/jones/v1/keymaps/jpskenn_narrow/keymap.c
@@ -295,13 +295,16 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 void keyboard_post_init_user(void) {
 
 #ifdef RGB_DI_PIN
-#ifdef RGBLIGHT_LAYERS
+  #ifdef RGBLIGHT_LAYERS
     // Enable the LED layers.
     rgblight_layers = my_rgb_layers;
 
     // prevent RGB light overrides layer indicator.
     layer_state_set(default_layer_state);
-#endif
+
+    // Set range to after layer indicator.
+    rgblight_set_effect_range(2, 15);
+  #endif
 #endif
 
 }
