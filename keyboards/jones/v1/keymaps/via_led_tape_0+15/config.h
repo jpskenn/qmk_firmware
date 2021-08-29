e3f9bb1e7fc5f3aec0003775a2c6193235348d99
Takeshi Nishio <kkeennnn@gmail.com>
2021-08-29 16:32:42 +0900

    Add "No Layer Indicator" keymap

---
+++ keyboards/jones/v1/keymaps/via_led_tape_0+15/config.h
@@ -0,0 +1,47 @@
+#pragma once
+
+//------------------------------------------------------------------------------
+// External EEPROM
+//------------------------------------------------------------------------------
+
+// Use 24LC64 EEPROM
+#define EEPROM_I2C_24LC64
+
+// Set MAX_ADDR to specify actual EEPROM size.
+// If it's not set, EEPROM size will be set ATmega32u4's 1023.
+#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 8191
+
+// How many layers to use with VIA / Remap.
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6 // default:4
+
+
+//------------------------------------------------------------------------------
+// RGB Lighting
+//------------------------------------------------------------------------------
+
+// Using Underglow LEDs(RGB Lighting)
+#ifdef RGB_DI_PIN
+  #undef RGBLED_NUM
+  #define RGBLED_NUM 15 // LED Strip(15*) *:60LEDs/1m
+
+  // LED Animations
+  #define RGBLIGHT_EFFECT_BREATHING
+  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  #define RGBLIGHT_EFFECT_KNIGHT
+  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  #define RGBLIGHT_EFFECT_TWINKLE
+
+  // No Lighting layer
+  #undef RGBLIGHT_LAYERS
+#endif
+
+//------------------------------------------------------------------------------
+// Audio
+//------------------------------------------------------------------------------
+
+// Disable "Music Mode" to reduce firmware size.
+#ifdef AUDIO_ENABLE
+  #define NO_MUSIC_MODE
+  #undef MUSIC_MAP
+#endif
---
+++ keyboards/jones/v1/keymaps/via_led_tape_0+15/readme.md
@@ -0,0 +1,44 @@
+# Keymap for Jones with VIA support
+
+No layer indicator.
+Underglow LED strip supprted.
+Based on "via" keymap.
+
+## Layout
+
+Japanese style wide layout.
+
+## Layers
+
+- Base #1
+- Base #2
+- Base #3
+- Lower
+- Raise
+- Adjust
+
+## External 8KB EEPROM
+
+I2C connected external 8KB EEPROM.
+
+## LED lighting
+
+- No layer indicator.
+- 15 LEDs for underglow LED strip.
+
+## Rotary encoder
+
+Supports up to 2 rotary encoders. (Left bottom and Right bottom.)  
+
+## Audio
+
+Supports audio.  
+
+To reduce firmware size, "Music Mode" and "Audio Click" are *disabled*.
+
+## Bootmagic
+
+The "Lite" mode is enabled.  
+Hold "Left-Top" key and plug USB cable to enter bootloader.  
+
+Note: EEPROM will be reset when use Bootmagic Lite.
