fadcc5941ae24977989eda25dd9b345c5dfa581a
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-10-05 18:38:01 +0900

    未指定だとLEDが明るすぎ＆発熱が懸念されるため、そこそこのデフォルト値を用意

--- keyboards/jones/v04_1/config.h
+++ keyboards/jones/v04_1/config.h
@@ -57,6 +57,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_DEFAULT_VAL 128
   /*== Lighting Layers ==*/
   #define RGBLIGHT_LAYERS
   #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF // Overriding RGB Lighting on/off status
