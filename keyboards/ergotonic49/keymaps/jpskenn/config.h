65a28128b28c71a1ff2841a7523be08c6e56a748
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-10-18 05:29:29 +0900

    RGB Lightがオフでもインジケータは動作させる

--- keyboards/ergotonic49/keymaps/jpskenn/config.h
+++ keyboards/ergotonic49/keymaps/jpskenn/config.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define RGBLIGHT_LAYERS_RETAIN_VAL // set Lighting Layer brightness equals to Underglow's value
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF // Overriding RGB Lighting on/off status
