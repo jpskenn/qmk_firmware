1f8bd280e792ff2c8e8031b55117e10577d6403b
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-10-18 12:09:38 +0900

    RGB Light関連の設定を追加

--- keyboards/ergotonic49/keymaps/jpskenn/config.h
+++ keyboards/ergotonic49/keymaps/jpskenn/config.h
@@ -2,3 +2,7 @@
 
 #define RGBLIGHT_LAYERS_RETAIN_VAL // set Lighting Layer brightness equals to Underglow's value
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF // Overriding RGB Lighting on/off status
+
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#define RGBLIGHT_DEFAULT_VAL 100
+
