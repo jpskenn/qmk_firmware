e3980ecdba3b212da00e2daf68c33dabd39e914c
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-11-23 07:58:13 +0900

    キーマップ調整中

--- keyboards/nora/v01/keymaps/jpskenn/keymap.c
+++ keyboards/nora/v01/keymaps/jpskenn/keymap.c
@@ -72,13 +72,12 @@ enum custom_keycodes {
 #define ALT_JA  LALT_T(KC_LANG1)
 #define ALT_EN  LALT_T(KC_LANG2)
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT(
         ESC_NUM,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,   KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,
            KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_BSPC,     KC_P7,    KC_P8,    KC_P9,
            C_ESC,      KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,    KC_ENT,      KC_P4,    KC_P5,    KC_P6,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     _______,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
         DM_PLY1,  DM_PLY2,    KC_LOPT,     KC_LCMD,      SP_LOW,             SP_RAI,         GUI_IME,    KC_ROPT,     KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT
     ),
     [_WIN] = LAYOUT(
