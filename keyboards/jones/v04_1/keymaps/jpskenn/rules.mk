e0426b9f770bc0b2c2bf9d4b4f7fce16cf40bee7
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-11-23 14:57:10 +0900

    未使用機能をOFFにしてファームウェアの空き容量を確保

--- keyboards/jones/v04_1/keymaps/jpskenn/rules.mk
+++ keyboards/jones/v04_1/keymaps/jpskenn/rules.mk
@@ -2,6 +2,10 @@ VIA_ENABLE = yes
 
 EEPROM_DRIVER = i2c
 
-MOUSEKEY_ENABLE = no // Set "no" to reduce firmware size
-
 DYNAMIC_MACRO_ENABLE = yes
+
+# Set "no" to reduce firmware size
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
