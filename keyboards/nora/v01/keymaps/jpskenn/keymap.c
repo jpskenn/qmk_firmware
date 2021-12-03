b3952e8a5db70faaeaefe3334770a98809e55a3a
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-12-03 20:45:16 +0900

    レイヤーキーの位置まちがい

--- keyboards/nora/v01/keymaps/jpskenn/keymap.c
+++ keyboards/nora/v01/keymaps/jpskenn/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_INS,      _______,  _______,  _______,
            _______,    _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,     KC_LBRC,     KC_RBRC,  KC_BSLS,  _______,  _______,  _______,
         _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,  _______,
-        _______,  _______,    _______,     _______,      _______,            _______,        SP_ADJ,     _______,     _______,  _______,  _______,  _______,  _______
+        _______,  _______,    _______,     _______,      _______,            SP_ADJ,         _______,    _______,     _______,  _______,  _______,  _______,  _______
     ),
     [_RAISE] = LAYOUT(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
