ef0823e3f97bd8737fc6b4242785821edf44267e
Takeshi Nishio <kkeennnn+github@gmail.com>
2021-12-29 05:43:48 +0900

    WIP JPの記号を配置中

--- keyboards/nora/v01/keymaps/duo/keymap.c
+++ keyboards/nora/v01/keymaps/duo/keymap.c
@@ -85,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DM_PLY1,  DM_PLY2,    KC_LOPT,     KC_LCMD,      SP_LO_U,             SP_RAI,         GUI_IME,    KC_ROPT,     KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT
     ),
     [_JP] = LAYOUT(
-        ESC_NUM,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     KC_EQL,   JP_6,     JP_7,     KC_8,     KC_9,     KC_0,     KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,
+        ESC_NUM,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_MINS,  JP_6,     JP_7,     KC_8,     KC_9,     KC_0,     KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,
            KC_TAB,     JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,       KC_BSPC,     KC_P7,    KC_P8,    KC_P9,
            C_ESC,      JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,    KC_ENT,      KC_P4,    KC_P5,    KC_P6,
-        KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     KC_1,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
+        KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     JP_BSLS,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
         DM_PLY1,  DM_PLY2,    KC_LOPT,     KC_LCMD,      SP_LO_J,             SP_RAI,         GUI_IME,    KC_ROPT,     KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT
     ),
     [_NUM] = LAYOUT(
@@ -100,16 +100,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LOWER_US] = LAYOUT(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-           KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_INS,      _______,  _______,  _______,
-           _______,    _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,     KC_LBRC,     KC_RBRC,  KC_BSLS,  _______,  _______,  _______,
+           KC_GRV,     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_INS,      _______,  _______,  _______,
+           _______,    _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,    KC_BSLS,     _______,  _______,  _______,
         _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,  _______,
         _______,  _______,    _______,     _______,      _______,            _______,        _______,    _______,     _______,  _______,  _______,  _______,  _______
     ),
-    [_LOWER_JP] = LAYOUT(
+    [_LOWER_JP] = LAYOUT(//TODO JPの記号をLower_JPに押し込む。変換、無変換とかも入れたい。MUTEを左へずらせば、そのすぐ右から全部入る？
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-           KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_INS,      _______,  _______,  _______,
-           _______,    _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,     KC_LBRC,     KC_RBRC,  KC_BSLS,  _______,  _______,  _______,
-        _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,  _______,
+           JP_ZKHK,    JP_EXLM,  JP_DQUO,  JP_HASH,  JP_DLR,   JP_PERC,  JP_AMPR,  JP_QUOT,  JP_LPRN,  JP_RPRN,  XXXXXXX,    KC_INS,      _______,  _______,  _______,
+           _______,    _______,  _______,  _______,  _______,  _______,  JP_MINS,  JP_CIRC,  JP_YEN,   KC_LBRC,  KC_RBRC,    KC_BSLS,  _______,  _______,  _______,
+        _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  JP_EQL,   JP_TILD,  JP_PIPE,    KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,  _______,
         _______,  _______,    _______,     _______,      _______,            _______,        _______,    _______,     _______,  _______,  _______,  _______,  _______
     ),
     [_LOWER_NUM] = LAYOUT(
