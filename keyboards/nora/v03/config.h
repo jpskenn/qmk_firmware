2c2cfd6911f9739a432cd7fc71a9adef1a09d856
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-02 05:48:46 +0900

    Move LED count inside "RGB Lighting" settings

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -39,7 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* RGBLIGHT */
 #define RGB_DI_PIN E6
 #ifdef RGB_DI_PIN
-  #define RGBLED_NUM 71
 
   #ifdef RGB_MATRIX_ENABLE
     #define DRIVER_LED_TOTAL 71
@@ -54,6 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     #define RGB_MATRIX_KEYPRESSES
   #else
+    #define RGBLED_NUM 71
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
