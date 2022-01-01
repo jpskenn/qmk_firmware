c2e042fb66f764e89967ca01b140fcf2c67beb97
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-02 05:49:17 +0900

    Readability

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -43,15 +43,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #ifdef RGB_MATRIX_ENABLE
     #define DRIVER_LED_TOTAL 71
     #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
-
     #define RGB_MATRIX_HUE_STEP 8
     #define RGB_MATRIX_SAT_STEP 8
     #define RGB_MATRIX_VAL_STEP 8
     #define RGB_MATRIX_SPD_STEP 8
-
     #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-
     #define RGB_MATRIX_KEYPRESSES
+
   #else
     #define RGBLED_NUM 71
     #define RGBLIGHT_HUE_STEP 8
