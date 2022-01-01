b6184e1470c4a85e6f902f1ba1473af826e4cc7b
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-02 05:35:49 +0900

    実際のピン割り当てに変更

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -32,8 +32,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 8
 
 // Same pins for Round-Robin matrix.
-#define MATRIX_ROW_PINS { F1, B7, D2, F6, F7, B0, F4, F5, C7, B6 }
-#define MATRIX_COL_PINS { F0, D3, D5, D4, D6, D7, B4, B5 }
+#define MATRIX_ROW_PINS { D3, D2, D7, B7, F1, D5, D4, D6, B4, F0 }
+#define MATRIX_COL_PINS { B0, C7, B6, B5, F7, F6, F5, F4 }
 #define DIODE_DIRECTION COL2ROW
 
 /* RGBLIGHT */
