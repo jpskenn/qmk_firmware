3e28ec01fd8234d57af150cc5016dc87984bc013
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-02 05:47:22 +0900

    commenting

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -59,7 +59,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_VAL_STEP 8
     #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
     #define RGBLIGHT_DEFAULT_VAL 127 /* The default brightness level */
-    /*== Lighting Layers ==*/
+    /* Lighting Layers */
     #define RGBLIGHT_LAYERS
     #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF // Overriding RGB Lighting on/off status
     #define RGBLIGHT_LAYERS_RETAIN_VAL // Keeps RGB values between lighting layer changes
