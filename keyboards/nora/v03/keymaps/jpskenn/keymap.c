d45b1ad6e69cdba63cae841be5e1523228de3a83
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-10 18:09:50 +0900

    R1をあまり使わないように変更

--- keyboards/nora/v03/keymaps/jpskenn/keymap.c
+++ keyboards/nora/v03/keymaps/jpskenn/keymap.c
@@ -74,11 +74,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT(
-        ESC_NUM,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
-        KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_P7,    KC_P8,    KC_P9,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
-        C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_P4,    KC_P5,    KC_P6,    KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     DM_PLY1,  KC_P1,    KC_P2,    KC_P3,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                 KC_LOPT,       KC_LCMD,     SP_LOW,     DM_PLY2,  KC_P0,    KC_PDOT,    SP_RAI,      GUI_IME,    KC_ROPT
+        ESC_NUM,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_GRV,   XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+        KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,  XXXXXXX,  KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
+        C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_SCLN,  XXXXXXX,  KC_QUOT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     DM_PLY1,  XXXXXXX,  XXXXXXX,  DM_PLY2,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                                 KC_LOPT,       KC_LCMD,     SP_LOW,     KC_BSPC,  KC_ESC,   KC_ENT,     SP_RAI,      GUI_IME,    KC_ROPT
     ),
     [_WIN] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
@@ -105,22 +105,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
         KC_ESC,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_DEL,
         _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_SCLN,       KC_QUOT,
-        _______,  _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,
-                                 _______,       _______,     _______,    _______,  _______,  _______,    _______,     _______,    _______
+        _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,
+                                 _______,       _______,     _______,    KC_F12,   _______,  _______,    _______,     _______,    _______
     ),
     [_NUM_LOWER] = LAYOUT(
-        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
-        KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_P7,    KC_P8,    KC_P9,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
-        C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_P4,    KC_P5,    KC_P6,    KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_PIPE,  KC_P1,    KC_P2,    KC_P3,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                 KC_LOPT,       KC_LCMD,     _______,    KC_P0,    KC_P0,    KC_PDOT,    _______,     GUI_IME,    KC_ROPT
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_GRV,   XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+        KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,  XXXXXXX,  KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
+        C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_SCLN,  XXXXXXX,  KC_QUOT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     DM_PLY1,  XXXXXXX,  XXXXXXX,  DM_PLY2,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                                 KC_LOPT,       KC_LCMD,     _______,    KC_BSPC,  KC_ESC,   KC_ENT,     _______,     GUI_IME,    KC_ROPT
     ),
     [_ADJUST] = LAYOUT(
-        DM_RSTP,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,       MAC,      WIN,      NUM,      _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_SAI,  RGB_VAI,  _______,  RGB_RMOD,      _______,
+        DM_RSTP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        DM_RSTP,       MAC,      WIN,      NUM,      _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_SAI,  RGB_VAI,  _______,  RGB_RMOD,      _______,
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_TOG,  RGB_MOD,       VERSION,
-        KC_CAPS,  _______,  _______,  _______,  _______,  _______,  DM_REC1,  _______,  _______,  _______,  NUM,      _______,  _______,  _______,  _______,  _______,
-                                 _______,       _______,     _______,    DM_REC2,  _______,  _______,    _______,     _______,    _______
+        KC_CAPS,  _______,  _______,  _______,  _______,  _______,  DM_REC1,  _______,  _______,  DM_REC2,  NUM,      _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,
+                                 _______,       _______,     _______,    _______,  _______,  _______,    _______,     _______,    _______
     ),
 };
 
