9ee3071d1ea8e54cab10d908cddc9bc896ad66ce
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-01-26 05:52:14 +0900

    自分用FWを追加。ロータリーエンコーダなし。

---
+++ keyboards/nora/v031/keymaps/jpskenn_fw/rules.mk
@@ -0,0 +1,20 @@
+VIA_ENABLE = yes
+
+DYNAMIC_MACRO_ENABLE = yes
+
+AUDIO_ENABLE = no
+ENCODER_ENABLE = no
+
+# Set "no" to reduce firmware size.
+# CONSOLE_ENABLE = no
+# COMMAND_ENABLE = no
+# MOUSEKEY_ENABLE = no
+# EXTRAKEY_ENABLE = no
+# SPACE_CADET_ENABLE = no
+# GRAVE_ESC_ENABLE = no
+# MAGIC_ENABLE = no
+
+# --- RGB Matrix ---
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
