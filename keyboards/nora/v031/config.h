b41e1df15cf083ba67a396aa8805bdd7bd136742
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-02-01 06:48:39 +0900

    Commenting

--- keyboards/nora/v031/config.h
+++ keyboards/nora/v031/config.h
@@ -143,6 +143,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_LAYERS_RETAIN_VAL // Keeps RGB values between lighting layer changes
     #define RGBLIGHT_MAX_LAYERS 10 // default: 8
     #define RGBLIGHT_LAYER_BLINK
+    /* Animations & Effects */
     #define RGBLIGHT_ANIMATIONS
     #define RGBLIGHT_EFFECT_TWINKLE
   #endif
@@ -160,7 +161,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Use 24LC64 EEPROM
 #define EEPROM_I2C_24LC64
 // Set MAX_ADDR to specify actual EEPROM size.
-// If it's not set, EEPROM size will be set ATmega32u4's 1023.
+// If it's not set, the size will be set to ATmega32u4's 1023.
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 8191
 // How many layers to use with VIA / Remap.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7 // default:4
