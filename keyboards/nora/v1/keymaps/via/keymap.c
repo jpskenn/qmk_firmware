5a733ce2b98da174a490c45c508c0be8e5845ae3
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-02-17 07:30:02 +0900

    Full layout

--- keyboards/nora/v1/keymaps/via/keymap.c
+++ keyboards/nora/v1/keymaps/via/keymap.c
@@ -55,37 +55,37 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE1] = LAYOUT_all(
-        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,   KC_PGDN,  KC_PGUP, JP_YEN,   JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     JP_MINS,
+        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,  KC_PGDN,  KC_PGUP,  JP_YEN,   JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     JP_MINS,
         KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_AT,    XXXXXXX,  JP_LBRC,  JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,          KC_BSPC,
-        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     JP_COLN,  KC_UP,    JP_RBRC,  JP_H,     JP_J,     JP_K,     JP_L,     JP_SCLN,       KC_ENT,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     JP_COLN,  KC_UP,    JP_RBRC,  JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,       KC_ENT,
         KC_LSFT,  KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     KC_LEFT,  KC_DOWN,  KC_RGHT,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,
                                  KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,  FUNC,     KC_ENT,   SP_RAI,      KC_RALT,    KC_APP
     ),
     [_BASE2] = LAYOUT_all(
-        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     KC_PSLS,   KC_PAST,  KC_PNS,  KC_PPLS,  JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     JP_MINS,
+        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,  JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     JP_MINS,
         KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     KC_P7,    KC_P8,    KC_P9,    JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,          KC_BSPC,
-        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     KC_P4,    KC_P5,    KC_P6,    JP_H,     JP_J,     JP_K,     JP_L,     JP_SCLN,       KC_ENT,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     KC_P4,    KC_P5,    KC_P6,    JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,       KC_ENT,
         KC_LSFT,  KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     KC_P1,    KC_P2,    KC_P3,    JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,
                                  KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,  KC_P0,    KC_PDOT,    SP_RAI,      KC_RALT,    KC_APP
     ),
     [_BASE3] = LAYOUT_all(
         JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,  KC_PGDN,  KC_PGUP,  JP_YEN,   JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     KC_MINS,
         KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_AT,    XXXXXXX,  JP_LBRC,  JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,          KC_BSPC,
-        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     JP_COLN,  KC_UP,    JP_RBRC,  JP_H,     JP_J,     JP_K,     JP_L,     JP_SCLN,       KC_ENT,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,     JP_COLN,  KC_UP,    JP_RBRC,  JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,       KC_ENT,
         KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RGHT,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,
-                                 KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,  FUNC,     KC_ENT,     SP_RAI,      KC_RALT,    KC_APP,
+                                 KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,  FUNC,     KC_ENT,     SP_RAI,      KC_RALT,    KC_APP
    ),
-    [_LOWER] = LAYOUT_all(//TODO JP shifted
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-        KC_ESC,        KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  _______,  _______,  _______,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,       _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,       KC_BSLS,
-        _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
+    [_LOWER] = LAYOUT_all(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+        KC_ESC,        JP_EXLM,  JP_DQUO,  JP_HASH,  JP_DLR,   JP_PERC,  JP_TILD,  _______,  JP_PIPE,  JP_AMPR,  JP_QUOT,  JP_LPRN,  JP_RPRN,  _______,       JP_EQL,
+        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  JP_AT,    JP_SCLN,  JP_COLN,  JP_LBRC,  JP_RBRC,       KC_BSLS,
+        _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,  _______,  JP_GRV,   JP_PLUS,  JP_ASTR,  JP_LCBR,  JP_RCBR,  KC_PIPE,
                                  _______,       _______,     _______,    _______,  _______,  _______,    ADJUST,      _______,    _______
     ),
     [_RAISE] = LAYOUT_all(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-        KC_ESC,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_DEL,
-        _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_SCLN,       KC_QUOT,
+        KC_ESC,        JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     _______,  _______,  _______,  JP_6,     JP_7,     JP_8,     JP_9,     JP_0,          KC_DEL,
+        _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  JP_SCLN,       JP_COLN,
         _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,
                                  _______,       _______,     ADJUST,     KC_F12,   _______,  _______,    _______,     _______,    _______
     ),
