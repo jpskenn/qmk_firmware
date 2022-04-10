c7542b5204f8141aea38aef31a1f742b93fc2224
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-04-10 09:13:24 +0900

    Increase layer count 7 --> 10

--- keyboards/nora/v1/keymaps/via_re/keymap.c
+++ keyboards/nora/v1/keymaps/via_re/keymap.c
@@ -15,6 +15,7 @@
  */
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "keymap_jp.h"
 
 void rgb_matrix_set_color_user(int, int, int, int, bool);
 
@@ -28,6 +29,9 @@ enum layer_number {
     _LOWER,
     _RAISE,
     _FUNC,
+    _RESERVE1,
+    _RESERVE2,
+    _RESERVE3,
     _ADJUST,
 };
 
@@ -53,60 +57,84 @@ enum custom_keycodes {
 #define ADJUST  MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE1] = LAYOUT(
-        KC_ESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_GRV,   XXXXXXX,  XXXXXXX,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,
-        KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,  XXXXXXX,  KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
-        C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_SCLN,  XXXXXXX,  KC_QUOT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
-        KC_LSFT,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  KC_MUTE,  KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  FUNC,
-                                 KC_LOPT,       KC_LCMD,     SP_LOW,     KC_BSPC,  FUNC,     KC_ENT,     SP_RAI,      KC_RGUI,    KC_ROPT,
+    [_BASE1] = LAYOUT_re(
+        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,                      JP_YEN,   JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     JP_MINS,
+        KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_AT,              JP_LBRC,  JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,          KC_BSPC,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,               KC_MUTE,            JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,       KC_ENT,
+        KC_LSFT,  KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,                         XXXXXXX,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,
+                                 KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,            KC_ENT,   SP_RAI,      KC_RALT,    KC_APP,
                                                                               KC_VOLD,  KC_VOLU
     ),
-    [_BASE2] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-                                 KC_LWIN,       KC_LALT,     _______,    _______,  _______,  _______,    _______,     KC_RALT,    KC_APP,
+    [_BASE2] = LAYOUT_re(
+        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,                      JP_YEN,   JP_6,     KC_PSLS,  KC_PAST,  KC_PMNS,  JP_0,     JP_MINS,
+        KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_AT,              JP_LBRC,  JP_Y,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS,       KC_BSPC,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,               KC_MUTE,            JP_H,     KC_P4,    KC_P5,    KC_P6,    JP_MINS,       KC_ENT,
+        KC_LSFT,  KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,                         XXXXXXX,  JP_N,     KC_P1,    KC_P2,    KC_P3,    JP_SLSH,  JP_BSLS,
+                                 KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,            KC_ENT,   SP_RAI,      KC_P0,      KC_PDOT,
+                                                                              KC_VOLD,  KC_VOLU
+    ),
+    [_BASE3] = LAYOUT_re(
+        JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_CIRC,                      JP_YEN,   JP_6,     JP_7,     JP_8,     JP_9,     JP_0,     KC_MINS,
+        KC_TAB,        JP_Q,     JP_W,     JP_E,     JP_R,     JP_T,     JP_AT,              JP_LBRC,  JP_Y,     JP_U,     JP_I,     JP_O,     JP_P,          KC_BSPC,
+        C_ESC,         JP_A,     JP_S,     JP_D,     JP_F,     JP_G,               KC_MUTE,            JP_H,     JP_J,     JP_K,     JP_L,     JP_MINS,       KC_ENT,
+        KC_LSFT,  JP_Z,     JP_X,     JP_C,     JP_V,     JP_B,     XXXXXXX,                      XXXXXXX,  JP_N,     JP_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,
+                                 KC_LWIN,       KC_LALT,     SP_LOW,     KC_BSPC,            KC_ENT,     SP_RAI,      KC_RALT,    KC_APP,
+                                                                              KC_VOLD,  KC_VOLU
+    ),
+    [_LOWER] = LAYOUT_re(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,                      KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+        KC_ESC,        JP_EXLM,  JP_DQUO,  JP_HASH,  JP_DLR,   JP_PERC,  JP_TILD,            JP_PIPE,  JP_AMPR,  JP_QUOT,  JP_LPRN,  JP_RPRN,  _______,       JP_EQL,
+        _______,       _______,  _______,  _______,  _______,  _______,            _______,            JP_AT,    JP_SCLN,  JP_COLN,  JP_LBRC,  JP_RBRC,       KC_BSLS,
+        _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,                      _______,  JP_GRV,   JP_PLUS,  JP_ASTR,  JP_LCBR,  JP_RCBR,  KC_PIPE,
+                                 _______,       _______,     _______,    _______,            _______,    ADJUST,      _______,    _______,
                                                                               _______,  _______
     ),
-    [_BASE3] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BSLS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-                                 _______,       _______,     _______,    _______,  _______,  _______,    _______,     _______,    _______,
+    [_RAISE] = LAYOUT_re(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,                      KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+        KC_ESC,        JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     _______,            _______,  JP_6,     JP_7,     JP_8,     JP_9,     JP_0,          KC_DEL,
+        _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              _______,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  JP_SCLN,       JP_COLN,
+        _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,                       _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,
+                                 _______,       _______,     ADJUST,     KC_F12,             _______,    _______,     _______,    _______,
                                                                               _______,  _______
     ),
-    [_LOWER] = LAYOUT(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-        KC_ESC,        KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  _______,  _______,  _______,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,       _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,       KC_BSLS,
-        _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
-                                 _______,       _______,     _______,    _______,  _______,  _______,    ADJUST,      _______,    _______,
+    [_FUNC] = LAYOUT_re(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,       _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,       _______,
+        _______,       _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______,  _______,       _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                 _______,       _______,     _______,    _______,            _______,    _______,     _______,    _______,
                                                                               _______,  _______
     ),
-    [_RAISE] = LAYOUT(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-        KC_ESC,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_DEL,
-        _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_SCLN,       KC_QUOT,
-        _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,
-                                 _______,       _______,     ADJUST,     KC_F12,   _______,  _______,    _______,     _______,    _______,
+    [_RESERVE1] = LAYOUT_re(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,       _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,       _______,
+        _______,       _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______,  _______,       _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                 _______,       _______,     _______,    _______,            _______,    _______,     _______,    _______,
                                                                               _______,  _______
     ),
-    [_FUNC] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-                                 _______,       _______,     _______,    _______,  _______,  _______,    _______,     _______,    _______,
+    [_RESERVE2] = LAYOUT_re(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,       _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,       _______,
+        _______,       _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______,  _______,       _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                 _______,       _______,     _______,    _______,            _______,    _______,     _______,    _______,
                                                                               _______,  _______
     ),
-    [_ADJUST] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,       BASE1,    BASE2,    BASE3,    _______,  _______,  _______,  _______,  RGB_SPI,  RGB_HUI,  RGB_SAI,  RGB_VAI,  IND_TOG,  RGB_RMOD,      KC_INS,
-        KC_CAPS,       AU_TOG,   MU_TOG,   MU_MOD,   MUV_DE,   MUV_IN,   _______,  _______,  RGB_SPD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_TOG,  RGB_MOD,       VERSION,
-        _______,  CK_TOGG,  CK_RST,   CK_DOWN,  CK_UP,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,
-                                 _______,       _______,     _______,    _______,  _______,  _______,    _______,     _______,    _______,
+    [_RESERVE3] = LAYOUT_re(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,       _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,       _______,
+        _______,       _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______,  _______,       _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                 _______,       _______,     _______,    _______,            _______,    _______,     _______,    _______,
+                                                                              _______,  _______
+    ),
+    [_ADJUST] = LAYOUT_re(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,       BASE1,    BASE2,    BASE3,    _______,  _______,  _______,            RGB_SPI,  RGB_HUI,  RGB_SAI,  RGB_VAI,  IND_TOG,  RGB_RMOD,      KC_INS,
+        KC_CAPS,       AU_TOG,   MU_TOG,   MU_MOD,   MUV_DE,   MUV_IN,             _______,            RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_TOG,  RGB_MOD,       VERSION,
+        _______,  CK_TOGG,  CK_RST,   CK_DOWN,  CK_UP,    _______,  _______,                      _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,
+                                 _______,       _______,     _______,    _______,            _______,    _______,     _______,    _______,
                                                                               _______,  _______
     ),
 };
@@ -189,12 +217,17 @@ void rgb_matrix_indicators_user(void) {
             rgb_matrix_set_color_user(LED_INDEX_LSPC, HSV_WHITE, true);
             rgb_matrix_set_color_user(LED_INDEX_RSPC, HSV_WHITE, true);
             break;
+        // No Indicator for Reserved layers.
+        // case _RESERVE1:
+        // case _RESERVE2:
+        // case _RESERVE3:
+        //     break;
         case _ADJUST:
             rgb_matrix_set_color_user(LED_INDEX_LSPC, HSV_RED, true);
             rgb_matrix_set_color_user(LED_INDEX_RSPC, HSV_RED, true);
             break;
         default:
-            // Do nothing to enable effect / animation color.
+            // No indicator to show effect / animation color.
             break;
     }
 
