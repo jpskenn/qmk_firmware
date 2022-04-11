b859b1b24e5ac5f9f4920b26e27a70cb1d16b758
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-04-11 14:53:34 +0900

    Update license headers

--- keyboards/nora/v1/config.h
+++ keyboards/nora/v1/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Takeshi Nishio
+Copyright 2022 Takeshi Nishio
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
