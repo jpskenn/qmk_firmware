85e37fbd1dffaca9b64c4cde30e84b8d6f46085c
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-05-08 21:18:29 +0900

    入力言語切り替えを変更

--- keyboards/nora/v031/keymaps/jpskenn_re/keymap.c
+++ keyboards/nora/v031/keymaps/jpskenn_re/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,  XXXXXXX,  KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_BSPC,
         C_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_SCLN,  XXXXXXX,  KC_QUOT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_MINS,       KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     DM_PLY1,  XXXXXXX,  XXXXXXX,  DM_PLY2,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                 KC_LOPT,       KC_LCMD,     SP_LOW,     KC_BSPC,  KC_ESC,   KC_ENT,     SP_RAI,      GUI_IME,    KC_ROPT,
+                                 KC_LOPT,       GUI_EN,      SP_LOW,     KC_BSPC,  KC_ESC,   KC_ENT,     SP_RAI,      GUI_JA,     KC_ROPT,
                                                                               KC_VOLD,  KC_VOLU
     ),
     [_WIN] = LAYOUT(
