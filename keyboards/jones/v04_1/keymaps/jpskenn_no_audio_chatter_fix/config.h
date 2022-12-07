9beadb0e836342d4a84b37ac47ec6ffa5a9b4877
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-12-07 12:34:40 +0900

    Add チャタリング防止設定込みのキーマップ

---
+++ keyboards/jones/v04_1/keymaps/jpskenn_no_audio_chatter_fix/config.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// Change LED count.
+#ifdef RGB_DI_PIN
+    #undef RGBLED_NUM
+    #define RGBLED_NUM 2 // Left(2), Right(2)
+#endif
+
+// Disable music mode to reduce firmware size.
+// +2.3kb
+// #ifdef AUDIO_ENABLE
+//   #undef MUSIC_MAP
+//   #define NO_MUSIC_MODE
+//   #define SONG_CAPS_ON SONG(CAPS_LOCK_ON_SOUND)
+//   #define SONG_CAPS_OFF SONG(CAPS_LOCK_OFF_SOUND)
+// #endif
+
+// Disable unused encoder pad to reduce firmware size.
+// Override keyboard's config.h : Left encoder only
+// +0.3kb
+// #undef ENCODERS_PAD_A
+// #undef ENCODERS_PAD_B
+// #define ENCODERS_PAD_A { F5 }
+// #define ENCODERS_PAD_B { F4 }
+
+// Lighting Layer
+#define RGBLIGHT_LAYER_BLINK // Enable Blink function
+#define RGBLIGHT_MAX_LAYERS 10 // Blinkで2レイヤー追加すると、デフォルトの8レイヤーを超えてしまうため、ここで定義
+
+#define RGBLIGHT_LAYERS_RETAIN_VAL // set Lighting Layer brightness equals to Underglow's value
+
+// Use 24LC64 EEPROM
+#define EEPROM_I2C_24LC64
+
+// Set MAX_ADDR to specify actual EEPROM size.
+// If it's not set, EEPROM size will be set ATmega32u4's 1023.
+#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 8191
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7 // default:4
+
+// チャタリング防止設定
+// 参考：　https://25keys.com/2022/02/10/debounce/
+// rules.mk にも防止設定を追加している
+#ifdef DEBOUNCE
+#undef DEBOUNCE
+#define DEBOUNCE 50
+#endif
---
+++ keyboards/jones/v04_1/keymaps/jpskenn_no_audio_chatter_fix/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE = yes
+
+EEPROM_DRIVER = i2c
+
+MOUSEKEY_ENABLE = no // Set "no" to reduce firmware size
+
+DYNAMIC_MACRO_ENABLE = yes
+
+AUDIO_ENABLE = no
+
+# チャタリング防止設定
+# https://25keys.com/2022/02/10/debounce/
+# config.hにも防止設定を追加している
+DEBOUNCE_TYPE = sym_eager_pk
