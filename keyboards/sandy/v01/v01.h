dd85dcac438285e4eaf9bc96803beaf0df50e55d
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-03-19 08:35:36 +0900

    ロータリーエンコーダの仮装キー割当てを、レイアウト内に取り込み

--- keyboards/sandy/v01/v01.h
+++ keyboards/sandy/v01/v01.h
@@ -49,7 +49,7 @@ Merge two rows into single row to make 5*16 matrix.
     k20,   k70, k21, k71, k22, k72, k23, k73, k24, k74, k25, k75, k26,    k76, \
     k30, k80, k31, k81, k32, k82, k33, k83, k34, k84, k35, k85, k36, k86, k37, \
     k40, k90,  k41, k91,   k92,   k43, k93, k44,   k94,   k95,  k46, k96, k47, \
-                                       k17,                                    \
+    k57, k97,                          k17,                          k42, k45, \
                                   k27, k87, k77,                               \
                                        k67                                     \
 ) \
@@ -58,11 +58,10 @@ Merge two rows into single row to make 5*16 matrix.
     {   k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17   }, \
     {   k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27   }, \
     {   k30,   k31,   k32,   k33,   k34,   k35,   k36,   k37   }, \
-    {   k40,   k41,   KC_NO, k43,   k44,   KC_NO, k46,   k47   }, \
-    {   k50,   k51,   k52,   k53,   k54,   k55,   k56,   KC_NO }, \
+    {   k40,   k41,   k42,   k43,   k44,   k45,   k46,   k47   }, \
+    {   k50,   k51,   k52,   k53,   k54,   k55,   k56,   k57 }, \
     {   k60,   k61,   k62,   k63,   k64,   k65,   k66,   k67   }, \
     {   k70,   k71,   k72,   k73,   k74,   k75,   k76,   k77   }, \
     {   k80,   k81,   k82,   k83,   k84,   k85,   k86,   k87   }, \
-    {   k90,   k91,   k92,   k93,   k94,   k95,   k96,   KC_NO }  \
+    {   k90,   k91,   k92,   k93,   k94,   k95,   k96,   k97 }  \
 }
-// {k57, k97} and {k42, k45} is assigned to Rotary encoder
