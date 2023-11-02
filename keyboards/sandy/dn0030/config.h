148c5e0a95fa9fc848ac6e729963c68a40cca7d8
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-11-03 08:34:04 +0900

    Move encoder config to data driven (qmk#19923)
    - 関連する#ifdef - #endifの記述も削除。
    - エンコーダーをキーマトリクスに割り当てる定義は残っている（後で対応予定）

--- keyboards/sandy/dn0030/config.h
+++ keyboards/sandy/dn0030/config.h
@@ -22,11 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { B0, B1, B2, B3, D2, D5, D3, D4 }
 #define DIODE_DIRECTION COL2ROW
 
-/* Rotary Encoder */
-#ifdef ENCODER_ENABLE
-#define ENCODERS_PAD_A { F1, B6 }
-#define ENCODERS_PAD_B { F4, B5 }
-
 // Encoder triggers specific key matrix position,
 // when turn knob clockwise or counterclockwise.
 // This is handy to assign key codes by VIA.
@@ -38,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // --> {k57, k97}, {k42, k45}
 #define ENCODERS_CW_KEY  { { 7, 5 }, { 2, 4} }
 #define ENCODERS_CCW_KEY { { 7, 9 }, { 5, 4} }
-#endif
 
 /* RGB LED */
 #define WS2812_DI_PIN B7
