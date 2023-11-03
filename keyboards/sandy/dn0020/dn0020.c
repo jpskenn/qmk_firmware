7ea17086f927a4223617de0ca4f1c6645c283711
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-11-03 10:46:20 +0900

    Move single LAYOUTs to data driven (qmk#20365)

--- keyboards/sandy/dn0020/dn0020.c
+++ keyboards/sandy/dn0020/dn0020.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "dn0020.h"
+#include QMK_KEYBOARD_H
 
 
 #ifdef ENCODER_ENABLE
