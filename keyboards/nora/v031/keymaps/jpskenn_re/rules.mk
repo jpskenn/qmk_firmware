34a30b144298de0863c165680bc9809aa79516a0
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-11-04 10:59:49 +0900

    remove unused comment block

--- keyboards/nora/v031/keymaps/jpskenn_re/rules.mk
+++ keyboards/nora/v031/keymaps/jpskenn_re/rules.mk
@@ -3,12 +3,3 @@ VIA_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 
 ENCODER_ENABLE = yes
-
-# Set "no" to reduce firmware size.
-# CONSOLE_ENABLE = no
-# COMMAND_ENABLE = no
-# MOUSEKEY_ENABLE = no
-# EXTRAKEY_ENABLE = no
-# SPACE_CADET_ENABLE = no
-# GRAVE_ESC_ENABLE = no
-# MAGIC_ENABLE = no
--- keyboards/nora/v1/keymaps/jpskenn_fw/rules.mk
+++ keyboards/nora/v1/keymaps/jpskenn_fw/rules.mk
@@ -1,12 +1,3 @@
 VIA_ENABLE = yes
 
 DYNAMIC_MACRO_ENABLE = yes
-
-# Set "no" to reduce firmware size.
-# CONSOLE_ENABLE = no
-# COMMAND_ENABLE = no
-# MOUSEKEY_ENABLE = no
-# EXTRAKEY_ENABLE = no
-# SPACE_CADET_ENABLE = no
-# GRAVE_ESC_ENABLE = no
-# MAGIC_ENABLE = no
