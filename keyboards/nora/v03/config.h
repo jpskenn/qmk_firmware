e544febf91ade72ef9d5f928839975f771947cab
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-11-04 09:51:05 +0900

    Remove NO_ACTION_MACRO and NO_ACTION_FUNCTION from keyboard config.h

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -71,10 +71,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-
 /* External EEPROM */
 // Use 24LC64 EEPROM
 #define EEPROM_I2C_24LC64
