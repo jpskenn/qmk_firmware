df749ac6f4fb2414c75a1283aadffab7224e08a7
Takeshi Nishio <kkeennnn+github@gmail.com>
2023-11-04 11:06:26 +0900

    remove unused comment block

--- keyboards/nora/v03/config.h
+++ keyboards/nora/v03/config.h
@@ -53,8 +53,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF // Overriding RGB Lighting on/off status
     #define RGBLIGHT_LAYERS_RETAIN_VAL // Keeps RGB values between lighting layer changes
     #define RGBLIGHT_EFFECT_RGB_TEST
-    //   #define RGBLIGHT_ANIMATIONS
-    //   #define RGBLIGHT_EFFECT_TWINKLE
   #endif
 
 #endif
