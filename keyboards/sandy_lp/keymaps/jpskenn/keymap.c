cc93c5db209142a607625e10f00ec9a4a9b5a4b3
Takeshi Nishio <kkeennnn+github@gmail.com>
2024-10-19 17:12:57 +0900

    rangeのendを示す5番目の引数は、そのインデックスを含めないため、5番目のLEDがセットされていなかったのを修正

--- keyboards/sandy_lp/keymaps/jpskenn/keymap.c
+++ keyboards/sandy_lp/keymaps/jpskenn/keymap.c
@@ -252,7 +252,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case LCTR_RST: // Reset LED counter to Zero.
             if (record->event.pressed) {
-                rgblight_setrgb_range(0, 0, 0, 0, 5);
+                rgblight_sethsv_range(0, 0, 0, 0, 6);
                 led_0_hue = 0;
                 led_1_hue = 0;
                 led_2_hue = 0;
