1dd419ab61bd46290b351b19f39f89ba88a8731d
jpskenn <kkeennnn@gmail.com>
2024-10-24 10:21:20 +0900

    `Ctrl+C`などを入力する際、先に押したCtrlを素早く離した時にも、2キー目が押された時点で発動するように変更。

--- keyboards/sandy_lp/keymaps/jpskenn/config.h
+++ keyboards/sandy_lp/keymaps/jpskenn/config.h
@@ -30,3 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUDIO_VOICES
 #define AUDIO_ENABLE_TONE_MULTIPLEXING
 #define AUDIO_TONE_MULTIPLEXING_RATE_DEFAULT 10
+
+/* Tap-Hold */
+#define HOLD_ON_OTHER_KEY_PRESS
