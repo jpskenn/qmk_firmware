27449c6a924b40bbee9b4246355470a39fbfe8dd
jpskenn <kkeennnn@gmail.com>
2026-03-02 10:05:22 +0900

    基本的なところから作成中

--- keyboards/aida/keymaps/default/keymap.c
+++ keyboards/aida/keymaps/default/keymap.c
@@ -3,22 +3,27 @@
 
 #include QMK_KEYBOARD_H
 
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
-    )
+
+[_BASE] = LAYOUT(
+
+    // ───── 1行目（5 + 5）─────
+    KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P,
+
+    // ───── 2行目（5 + 5）─────
+    KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+
+    // ───── 3行目（6 + 6）─────
+    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ESC,   KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+
+    // ───── 4行目（4 + 4）─────
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,   KC_BSPC, KC_RALT, KC_RGUI, KC_RCTL
+)
+
 };
