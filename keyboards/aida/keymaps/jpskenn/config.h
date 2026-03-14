f25458459f2cf046c89797ffc200d90d9c8337f9
Takeshi Nishio <kkeennnn+github@gmail.com>
2026-03-14 15:23:19 +0900

    とりあえずEE_HANDS使わない

--- keyboards/aida/keymaps/jpskenn/config.h
+++ keyboards/aida/keymaps/jpskenn/config.h
@@ -35,5 +35,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define HOLD_ON_OTHER_KEY_PRESS
 // #define CHORDAL_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-
-#define EE_HANDS
