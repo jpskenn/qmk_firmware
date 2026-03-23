3baa2041c0b3a989cd9421f55ff00f89655750f3
jpskenn <kkeennnn@gmail.com>
2026-03-23 10:43:13 +0900

    キーマップ調整
    IMEのCtrl+UIOPの反応が悪くなるため

--- keyboards/aida/keymaps/jpskenn/keymap.c
+++ keyboards/aida/keymaps/jpskenn/keymap.c
@@ -89,7 +89,6 @@ enum custom_keycodes {
 
 // key code macros
 #define Q_NUM   LT(_NUM, KC_Q)
-#define P_NUM   LT(_NUM, KC_P)
 #define TAB_NUM   LT(_NUM, KC_TAB)
 #define MINS_NUM    LT(_NUM, KC_MINS)
 
@@ -129,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE1] = LAYOUT(
 //----+---------+---------+---------+---------+---------+----| |----+---------+---------+---------+---------+---------+----//
-       Q_NUM,    KC_W,     KC_E,     KC_R,     KC_T,                 KC_Y,     KC_U,     KC_I,     KC_O,     P_NUM,
+       Q_NUM,    KC_W,     KC_E,     KC_R,     KC_T,                 KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
 //----+---------+---------+---------+---------+---------+----| |----+---------+---------+---------+---------+---------+----//
        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     MINS_NUM,
 //---------+---------+---------+---------+---------+---------| |---------+---------+---------+---------+---------+---------//
