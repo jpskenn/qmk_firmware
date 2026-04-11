ca2cd3756c559e386b1ac91ebbd8893e4dd6ce2a
Takeshi Nishio <kkeennnn+github@gmail.com>
2026-04-11 13:12:04 +0900

    デバイスの動作を確認

--- keyboards/aida/keymaps/jpskenn/config.h
+++ keyboards/aida/keymaps/jpskenn/config.h
@@ -35,3 +35,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define HOLD_ON_OTHER_KEY_PRESS
 // #define CHORDAL_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
+/* I2C */
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP20
+#define I2C1_SCL_PIN GP21
+
+/* Haptic Feedback */
+// LRA settings
+#define DRV2605L_FB_ERM_LRA 1
+#define DRV2605L_FB_BRAKEFACTOR 3 /* For 1x:0, 2x:1, 3x:2, 4x:3, 6x:4, 8x:5, 16x:6, Disable Braking:7 */
+#define DRV2605L_FB_LOOPGAIN 1 /* For  Low:0, Medium:1, High:2, Very High:3 */
+
+/* Please refer to your datasheet for the optimal setting for your specific motor. */
+#define DRV2605L_RATED_VOLTAGE 1.8
+#define DRV2605L_V_PEAK 1.85
+#define DRV2605L_V_RMS 1.8
+#define DRV2605L_F_LRA 210 /* resonance freq */
+
+#define DRV2605L_GREETING 10
--- keyboards/aida/mcuconf.h
+++ keyboards/aida/mcuconf.h
@@ -26,3 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP_SIO_USE_UART0 TRUE
 #undef RP_SIO_USE_UART1
 #define RP_SIO_USE_UART1 FALSE
+
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
