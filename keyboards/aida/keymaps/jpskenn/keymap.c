671bd33e9642a4fbd56e73929b173bf749aacaeb
jpskenn <kkeennnn@gmail.com>
2026-05-13 15:59:23 +0900

    Haptic対象をオーバーライド

--- keyboards/aida/keymaps/jpskenn/keymap.c
+++ keyboards/aida/keymaps/jpskenn/keymap.c
@@ -747,6 +747,77 @@ bool led_update_user(led_t led_state) {
 #endif
 
 // ------------------------------------------------------------------------------
+// Haptic
+// ------------------------------------------------------------------------------
+// Override: process_haptic.c
+// TODO 自分の環境に合わせてオーバーライドしてカスタマイズ。
+// TODO 特に、LSFT_T(KC_SPC)とかで発動してしまうのを抑制したい。
+bool get_haptic_enabled_key(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+#ifdef NO_HAPTIC_MOD
+        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            if (record->tap.count == 0) return false;
+            break;
+        case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+            if (record->tap.count != TAPPING_TOGGLE) return false;
+            break;
+        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+            if (record->tap.count == 0) return false;
+            break;
+        case KC_LEFT_CTRL ... KC_RIGHT_GUI:
+        case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+        case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
+#endif
+#ifdef NO_HAPTIC_ALPHA
+        case KC_A ... KC_Z:
+#endif
+#ifdef NO_HAPTIC_PUNCTUATION
+        case KC_ENTER:
+        case KC_ESCAPE:
+        case KC_BACKSPACE:
+        case KC_SPACE:
+        case KC_MINUS:
+        case KC_EQUAL:
+        case KC_LEFT_BRACKET:
+        case KC_RIGHT_BRACKET:
+        case KC_BACKSLASH:
+        case KC_NONUS_HASH:
+        case KC_SEMICOLON:
+        case KC_QUOTE:
+        case KC_GRAVE:
+        case KC_COMMA:
+        case KC_SLASH:
+        case KC_DOT:
+        case KC_NONUS_BACKSLASH:
+#endif
+#ifdef NO_HAPTIC_LOCKKEYS
+        case KC_CAPS_LOCK:
+        case KC_SCROLL_LOCK:
+        case KC_NUM_LOCK:
+#endif
+#ifdef NO_HAPTIC_NAV
+        case KC_PRINT_SCREEN:
+        case KC_PAUSE:
+        case KC_INSERT:
+        case KC_DELETE:
+        case KC_PAGE_DOWN:
+        case KC_PAGE_UP:
+        case KC_LEFT:
+        case KC_UP:
+        case KC_RIGHT:
+        case KC_DOWN:
+        case KC_END:
+        case KC_HOME:
+#endif
+#ifdef NO_HAPTIC_NUMERIC
+        case KC_1 ... KC_0:
+#endif
+            return false;
+    }
+    return true;
+}
+
+// ------------------------------------------------------------------------------
 // Keyboard Initialization
 // ------------------------------------------------------------------------------
 void keyboard_post_init_user(void) {
